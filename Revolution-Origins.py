41e42dacea8297e07de0f72de0e63781aaeab8b5
BuildTools <unconfigured@null.spigotmc.org>
2021-11-13 17:29:39 +0100

    Writing of the Part 1

--- Revolution-Origins.py
+++ Revolution-Origins.py
@@ -20,13 +20,16 @@ print('Where am I?')
 name = input('Why am I here? Wait... WHO am I? Ah! I start to remember now...My name is \n')
         ###passt so weit
 print('What should I do now?')
-print('Should I go deeper in the Cave or should I go outside?')
-Cave_or_Outside = input('1: Outside\nor\n2: Cave\n')
-if Cave_or_Outside == "1":
+print('Should I look around in the Cave or should I go outside?')
+Look_or_Outside = input('1: Outside\nor\n2: Cave\n')
+if Look_or_Outside == "1":
     death = False
 else:
     death = True
-
+def stocke():
+    global
+    print("Should I go in the Cave or should I go get sticks?")
+     = input('1: Cave\nor\n2: Sticks\n')
 #1.1:Rausgehen    -Stöcke holen-Waffe (Faustkeil) bauen/zurück in die Höhle gehen
 
 #1.2.: In Höhle umsehen - Steine/Tiefer in die Höhle gehen-tot
