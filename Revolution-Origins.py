808c11faa432656f465332dab9f7b19afd9d42ba
Neo2460 <katharina.melchart@gmx.at>
2021-11-13 16:48:40 +0100

    Datei (Skizze) hinzugefügt

---
+++ Revolution-Origins.py
@@ -0,0 +1,29 @@
+'''
+date:13.11.2021
+authors: Bernhard Wolf
+         Katharina Melchart
+Skizze Our Origins
+'''
+
+#inports:
+import random as r
+
+
+
+#1 Dunkle Höhle
+#alles ist schwarz, bis text erscheint:
+print('Where am I?')
+name = input('Why am I here? Wait... WHO am I? Ah! I start to remember now...My name is \n')
+        ###passt so weit
+print('What should I do now?')
+#Die Knöpfen mit den 2 Entscheidungen erscheinen
+
+#1.1: Rausgehen-Stöcke holen-Waffe (Faustkeil) bauen/zurück in die Höhle gehen
+
+#1.2.: In Höhle umsehen - Steine/Tiefer in die Höhle gehen-tot
+
+#2 Draußen
+
+#2.1: Fallgrube-Beim Drübergehen draufgehen
+#2.2: Sammeln: gute Beeren/tödliche Pilze-tot
+
