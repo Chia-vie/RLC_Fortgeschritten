07572877f092affef5ef0ecd4b7c84bea1b412b6
Chia-vie <christine.ackerl@univie.ac.at>
2021-11-15 11:30:37 +0100

    RSA_stuff

--- Infos/RSA_encryption/Password_Encryption.py
+++ Infos/RSA_encryption/Password_Encryption.py
@@ -12,11 +12,11 @@ password = 'abcd'
 
 encrypted = []
 for letter in password:
-    encrypted.append(encrypt(ord(letter), 7, 91687))
+    encrypted.append(encrypt(ord(letter), 5, 1279151))
 
 decrypted = []
 for num in encrypted:
-    dec_num = decrypt(num, 26023, 91687)
+    dec_num = decrypt(num, 510749, 1279151)
     letter = chr(dec_num)
     decrypted.append(letter)
 
@@ -28,3 +28,6 @@ print(decrypted)
 
 #public_keys:{'e': 7, 'n': 91687}
 #private_keys:{'d': 26023, 'n': 91687}
+
+#public_keys:{'e': 5, 'n': 1279151}
+#private_keys:{'d': 510749, 'n': 1279151}
--- Infos/RSA_encryption/Text_Encryption.py
+++ Infos/RSA_encryption/Text_Encryption.py
@@ -5,14 +5,11 @@ def encrypt(message, e, n):
 def decrypt(message, d, n):
     return ((message ** d) % n)
 
-def text_to_num():
-    pass
+text = ord('a')
 
-text = ord('1')
+enc = encrypt(text, 5, 1279151)
 
-enc = encrypt(text, 7, 91687)
-
-print(chr(decrypt(enc, 26023, 91687)))
+print(f'Das ist deine Originaltext : {chr(decrypt(enc, 510749, 1279151))}')
 
 
 #public_keys:{'e': 5, 'n': 822649}
@@ -20,3 +17,6 @@ print(chr(decrypt(enc, 26023, 91687)))
 
 #public_keys:{'e': 7, 'n': 91687}
 #private_keys:{'d': 26023, 'n': 91687}
+
+#public_keys:{'e': 5, 'n': 1279151}
+#private_keys:{'d': 510749, 'n': 1279151}
