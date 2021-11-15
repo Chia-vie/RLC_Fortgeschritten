32cca1e3159fbf53c0616f92facb963441ac66fd
Chia-vie <christine.ackerl@univie.ac.at>
2021-11-15 11:03:19 +0100

    added password_encryption

---
+++ Infos/RSA_encryption/Password_Encryption.py
@@ -0,0 +1,30 @@
+def encrypt(message, e, n):
+    return ((message ** e) % n)
+
+
+def decrypt(message, d, n):
+    return ((message ** d) % n)
+
+def text_to_num():
+    pass
+
+password = 'abcd'
+
+encrypted = []
+for letter in password:
+    encrypted.append(encrypt(ord(letter), 7, 91687))
+
+decrypted = []
+for num in encrypted:
+    dec_num = decrypt(num, 26023, 91687)
+    letter = chr(dec_num)
+    decrypted.append(letter)
+
+print(decrypted)
+
+
+#public_keys:{'e': 5, 'n': 822649}
+#private_keys:{'d': 656669, 'n': 822649}
+
+#public_keys:{'e': 7, 'n': 91687}
+#private_keys:{'d': 26023, 'n': 91687}
